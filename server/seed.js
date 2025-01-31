3d506944c4aa182e1f44d5431ad2d55e90683569
jae <jl_135@outlook.com>
2025-01-31 23:04:08 +0000

    database url

--- server/seed.js
+++ server/seed.js
@@ -1,2 +1,2 @@
-DATABASE_URL="db.ccdjgrofprwdlfldpffw.supabase.co:5432/postgres"
-DATABASE_PASSWORD="@aws-0-eu-west-2.pooler.supabase.com:6543/postgres"
+DATABASE_URL="db.ccdjgrofprwdlfldpffw.supabase.co:5432/postgres";
+DATABASE_PASSWORD="@aws-0-eu-west-2.pooler.supabase.com:6543/postgres"'
