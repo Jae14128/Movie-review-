48ad7b9d617cef1b5386c6cc85be985f5dfa3572
jae <jl_135@outlook.com>
2025-02-02 20:57:36 +0000

    handleSubmit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,45 @@
-import css from "./App.css";
+
+import { useState } from "react";
+import appcss from "App.css";
+
 
 
 
 
 export default function App() {
-    return (
-        <>
-        <h1>App</h1>
-        {/*routes go here*/}
-        </>
-    );
-}
+    
+    const [feedback1, setFeedback2] = useState("");
+    const [feedback2, setFeedback1]= useState(['I watched Moana 2 and I think it was amazing- John',
+                                             'The Wallace and Gromit movie became a personal favourite, I would definitely recommend- Alison',
+                                             'I saw Wild Robot, it was not what I expected, but it was still a good watch- Naomi' ]);
+
+   
+   const handleSubmit = (event) => {
+    event.preventDefault();
+    setFeedback2( otherFeedback => [...otherFeedback, feedback1]);
+    setFeedback1('');
+   }
+
+   return (
+    <div className="App">
+        
+
+<br></br>
+<form onSubmit={handleSubmit}>
+    <div> Add your movie review</div>
+    <input type="text" value={feedback1} onChange={(e) => setFeedback1(e.target.value)}/>
+    <input type= "submit"/>
+    </form>
+
+    <br></br>
+    <h1>All reviews</h1>
+
+    <ul>
+        {feedback2.map((feedback2, index) => <li key={index}>{feedback2}</li>)}
+    </ul>
+    </div>
+    
+   );
+
+};
+    
