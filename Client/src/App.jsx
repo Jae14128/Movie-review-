c1865699cf4b08dd5769907c828609d5ab1f5330
jae <jl_135@outlook.com>
2025-02-02 22:01:16 +0000

    db images

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -18,17 +18,26 @@ export default function App() {
 
    return (
     <div className="App">
+
+        <img src="https://images.justwatch.com/poster/323606360/s166/moana-2" alt="Moana 2 movie advert"></img>
+        <img src="https://images.justwatch.com/poster/322033435/s166/mufasa-the-lion-king.avif" alt="Mufasa:Lion King movie advert"></img>
+        <img src="https://images.justwatch.com/poster/317505587/s166/the-wild-robot.avif" alt="Wild Robot movie advert"></img>
+        <img src="https://images.justwatch.com/poster/321462568/s166/wallace-and-gromit-vengeance-most-fowl.avif" alt="Wallace and Gromit movie advert"></img>
+        <img src="https://images.justwatch.com/poster/317102237/s166/inside-out-2.avif" alt="Inside Out 2 movie advert"></img>
+        <img src="https://images.justwatch.com/poster/320527460/s166/flow-2024.avif" alt="Flow movie advert"></img>
         
 
 <br></br>
 <form onSubmit={handleSubmit}>
-    <div> Add your movie review</div>
+    
     <input type="text" value={feedback1} onChange={(e) => setFeedback1(e.target.value)}/>
+    
     <input type= "submit"/>
     </form>
 
+    <br></br>
+    <h1>All Reviews</h1>
     
-    <h1><br>All Reviews</br></h1>
 
     <ul>
         {feedback2.map((feedback2, index) => <li key={index}>{feedback2}</li>)}
